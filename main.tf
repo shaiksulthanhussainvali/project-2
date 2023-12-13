972b193f8a6532e3f332b12283523e891c97695a
shaiksulthanhussainvali <140703649+shaiksulthanhussainvali@users.noreply.github.com>
2023-12-13 11:58:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-# Configure the AWS Provider
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA2XJAUE43MTYI6C6N"
-  secret_key = "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"
-}
 
 # VPC
 resource "aws_vpc" "project-2" {
